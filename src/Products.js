4c431490a70da4ae56de172fab59e03620a80862
Eitan <eitanavivi18@walla.com>
2021-04-12 20:19:08 +0300

    first commit

---
+++ src/Products.js
@@ -0,0 +1,16 @@
+import './Products.css'
+import Product from './Product'
+
+function Products(){
+    return(<section class="products">
+        <Product/> 
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>   
+    </section>
+
+    );
+}
+export default Products;
