a5be515ffa88a5fdf0fd8c873c4ef367b731a4de
Eitan <eitanavivi18@walla.com>
2021-05-03 12:44:50 +0300

    adding fetch and cart by hooks

--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,36 @@
 import './Header.css';
-function Header({categories, choose}){
-    return(
-      <nav class="product-filter">
-        <h1>Products</h1>
-  
-        <div class="sort">
-          <div class="collection-sort">
-            <label>Filter by:</label>
-            <select onChange= {(e) => {choose((e.target.value))}}>
+function Header({ categories, choose }) {
+  return (
+    <nav className="product-filter">
+      <h1>All products</h1>
+
+      <div className="sort">
+        <div className="collection-sort">
+          <label>Filter by:</label>
+          <select onChange={(e) => { choose((e.target.value)) }}>
             <option value="All Categories">All Categories</option>
 
-              {categories.map((category) => <option>{category}</option>)}
-              
-            </select>
-          </div>
-  
-          <div class="collection-sort">
-            <label>Sort by:</label>
-            <select>
-              <option value="/">Featured</option>
-              <option value="/">Best Selling</option>
-              <option value="/">Alphabetically, A-Z</option>
-              <option value="/">Alphabetically, Z-A</option>
-              <option value="/">Price, low to high</option>
-              <option value="/">Price, high to low</option>
-              <option value="/">Date, new to old</option>
-              <option value="/">Date, old to new</option>
-            </select>
-          </div>
+            {categories.map((category) => <option key={category}> {category}</option>)}
+
+          </select>
         </div>
-      </nav>
- 
-    );
-  }
+
+        <div className="collection-sort">
+          <label>Sort by:</label>
+          <select>
+            <option value="/">Featured</option>
+            <option value="/">Best Selling</option>
+            <option value="/">Alphabetically, A-Z</option>
+            <option value="/">Alphabetically, Z-A</option>
+            <option value="/">Price, low to high</option>
+            <option value="/">Price, high to low</option>
+            <option value="/">Date, new to old</option>
+            <option value="/">Date, old to new</option>
+          </select>
+        </div>
+      </div>
+    </nav>
+
+  );
+}
 export default Header;
--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,22 @@
-import './Product.css'
+import './Product.css';
+import ProductContext from "./ProductContext";
+import { useContext } from "react";
 
 function Product({title,price,description,image}){
+
+    const { addToCart } = useContext(ProductContext);
     return ( 
       
       <div className="product-card">
         <div className="product-image">
           <img
-            src={image} title={title}  
+            src={image} title={title} alt={description} 
           />
         </div>
         <div className="product-info">
           <h5>{title}</h5>
           <h6>${price}</h6>
+          <button onClick={()=>addToCart(title)}> + Add to cart + </button>
         </div>
       </div>
 
